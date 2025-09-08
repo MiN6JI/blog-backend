f304059280b8a8d279793fca4da5215c4a2c1e3f
MINGJI <deorukhkarshruti232@gmail.com>
2025-09-08 10:53:55 +0530

    Changes

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,14 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class PostSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run()
+    public function run(): void
     {
-        //
+        Post::create([
+            'title' => 'Welcome to My Blog!',
+            'body' => 'This is the first post created by the seeder. You can edit or delete it once your site is live.',
+            'tag' => 'general',
+            'feature_image' => null,
+            'user_id' => 1,
+        ]);
     }
 }
