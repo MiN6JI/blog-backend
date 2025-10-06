92138dfd7160ec3b4f1e89cbb3bcad7e6544e4d6
MINGJI <deorukhkarshruti232@gmail.com>
2025-10-06 14:52:19 +0530

    Changes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Blog\Http\Controllers\BlogController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('blogs', BlogController::class)->names('blog');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
