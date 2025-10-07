233dd64f5e355336e817e9d72174ac02059460c4
MINGJI <deorukhkarshruti232@gmail.com>
2025-10-07 17:06:27 +0530

    Changes

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL'), 'https://mingji-blogs.netlify.app', 'http://localhost:3000'],
+    'allowed_origins' => [env('FRONTEND_URL'), 'https://mingji-blogs.netlify.app', 'https://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
