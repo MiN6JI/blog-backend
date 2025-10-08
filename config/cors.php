63d45be0660bedabd67dab65097bdf82ffe59144
MINGJI <deorukhkarshruti232@gmail.com>
2025-10-08 11:48:03 +0530

    Changes

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL'), 'https://mingji-blogs.netlify.app', 'https://localhost:3000'],
+    'allowed_origins' => [env('FRONTEND_URL'), 'https://mingji-blogs.netlify.app', 'https://localhost:3000', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
